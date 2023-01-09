d3341327455ecd17643c222d02f8cf36b0096a69
Bishnu <thapabishnu20@gmail.com>
2023-01-09 21:00:16 +0545

    tip calculator

---
+++ Day 2/tip_calculator.py
@@ -0,0 +1,7 @@
+# Tip Calculator
+
+print("Welcome to the tip calculator.")
+total_bill = input("What was the toal bill ? $ ")
+people = input("How many people to split the bill ? ")
+tip = input("What percentage tip would you like to give? 10, 12 of 15? ")
+print("Each person sould pay")
