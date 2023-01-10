efe1a8d61c91eee61578b73505060f4222a31f9c
Bishnu <thapabishnu20@gmail.com>
2023-01-10 15:21:19 +0545

    day 3- sum of two digit number

---
+++ Day 3/sum_of_two_digit.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+sum = first_digit+second_digit
+# print(type(two_digit_number))
+print(f"Sum of two digit number is {sum}")
